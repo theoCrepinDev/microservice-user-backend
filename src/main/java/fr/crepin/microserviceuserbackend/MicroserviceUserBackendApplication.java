9860ad381cd6614cc2874e1338bb5e25e472fb23
Théo CREPIN <theo.crepin@efrei.net>
2023-03-23 13:17:32 +0100

    Feat : init Spring

---
+++ src/main/java/fr/crepin/microserviceuserbackend/MicroserviceUserBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.crepin.microserviceuserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUserBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceUserBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/crepin/microserviceuserbackend/MicroserviceUserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.crepin.microserviceuserbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
